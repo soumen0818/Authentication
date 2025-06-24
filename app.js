560a5c10c3811ce31ea0373a5d999adf635bdce9
soumen0818 <sdas721444@gmail.com>
2025-06-24 10:09:39 +0530

    Set up initial project structure with Express and Mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cokieParser = require('cookie-parser');
+const userModel = require("./models/user.js");
 
 
 
@@ -13,6 +14,14 @@ app.use(cokieParser());
 
 app.get('/', (req,res) => {
     res.render('\index');
+});
+
+app.post('/create',async (req,res) =>{
+    let{username, email, password, age} =req.body;
+    let createduser = await userModel.create({
+      username,email,password,age
+    })
+    res.send('createduser');
 });
 
 app.listen (3000);
