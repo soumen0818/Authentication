89264c8243fff8e9f99d356eb58323b9c6b7ca35
soumen0818 <sdas721444@gmail.com>
2025-06-24 11:19:34 +0530

    Implement user registration and login functionality with JWT authentication

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const cokieParser = require('cookie-parser');
 const userModel = require("./models/user.js");
+const bcrypt = require ('bcrypt');
+const { hash } = require('crypto');
+const jwt = require ('jsonwebtoken');
 
 
 
@@ -16,12 +19,50 @@ app.get('/', (req,res) => {
     res.render('\index');
 });
 
-app.post('/create',async (req,res) =>{
+app.post('/create', (req,res) =>{
     let{username, email, password, age} =req.body;
-    let createduser = await userModel.create({
-      username,email,password,age
+     
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) =>{
+
+             let createduser = await userModel.create({
+              username,
+              email,
+              password : hash,
+              age
+            })
+            
+            let token = jwt.sign({email}, "hsdgfghsjkiweuyrg");
+            res.cookie("token", token); 
+            res.send(createduser);
+        })
+        
     })
-    res.send('createduser');
+
 });
+
+app.get ("/logout", (req, res) =>{
+    res.cookie("token", "");
+    res.redirect('/');
+})
+app.get("/login", (req, res) =>{
+    res.render('login');
+})
+app.post("/login",async (req, res) =>{
+    let user = await userModel.findOne({email: req.body.email});
+    if(!user){
+        return res.send ("something is wrong");
+    }
+    bcrypt.compare (req.body.password, user.password, (err, result) =>{
+       if (result){
+        let token = jwt.sign({email: user.email}, "hsdgfghsjkiweuyrg");
+        res.cookie("token", token); 
+        res.send('yes you can login');
+       } 
+    });
+    
+});
+
+
 
 app.listen (3000);
